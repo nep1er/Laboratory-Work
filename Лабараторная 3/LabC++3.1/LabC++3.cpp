74fa6c77147e869541a75b7071e548baceae4be0
Nepler <151768633+nep1er@users.noreply.github.com>
2024-12-27 18:56:27 +0700

    Lab 3

---
+++ Лабараторная 3/LabC++3.1/LabC++3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ConvertB(int N, int b) 
+{
+    string result = "";
+    const char root[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    
+    while (N > 0) 
+    {
+        int rem = N % b;
+        result = root[rem] + result;
+        N /= b;
+    }
+    
+    return result == "" ? "0" : result;
+}
+
+int main()
+{
+    int b, N;
+    cin >> b;
+    cin >> N;
+
+    if (b < 2 || b > 36) 
+    {
+        cout << "Error 1" << endl;
+        return 1;
+    }
+
+    string result = ConvertB(N, b);
+    cout << result << endl;
+    
+    return 0;
+}
---
+++ Лабараторная 3/LabC++3.2/LabC++3.2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ConvertB(string N, int b) 
+{
+    int result = 0;
+    const char root[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    for (char c : N) 
+    {
+        int value;
+        if (c >= '0' && c <= '9') 
+        {
+            value = c - '0';
+        }
+        else if (c >= 'a' && c <= 'z') 
+        {
+            value = c - 'a' + 10;
+        }
+        else 
+        {
+            cout << "Error 2" << endl;
+            return -1;
+        }
+
+        if (value >= b) 
+        {
+            cout << "Error 3" << endl;
+            return -1;
+        }
+
+        result = result * b + value;
+    }
+
+    return result;
+}
+
+int main() 
+{
+    int b;
+    string N;
+
+    cin >> b;
+    cin >> N;
+
+    if (b < 2 || b > 16) 
+    {
+        cout << "Error 1" << endl;
+        return 1;
+    }
+
+    int decValue = ConvertB(N, b);
+    if (decValue == -1)
+    {
+        return 1;
+    }
+
+    cout << decValue << endl;
+
+    return 0;
+}
