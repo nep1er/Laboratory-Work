4afe3c0b3d6702a753a6d6897c6317165b26d227
Nepler <151768633+nep1er@users.noreply.github.com>
2024-12-27 18:57:58 +0700

    Lab 3 2

---
+++ Лабараторная 3/LabC++3.3/LabC++3.3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string ConvertB(double N, int b, int y)
+{
+    string result = "0.";
+    const char roots[] = "0123456789abcdef";
+
+    for (int i = 0; i < y; ++i) 
+    {
+        N *= b;
+        int root = static_cast<int>(N);
+        result += roots[root];
+        N -= root;
+        if (N == 0) break;
+    }
+
+    return result;
+}
+
+int main() 
+{
+    int b, y;
+    double N;
+    cin >> b;
+    cin >> y;
+
+    if (b < 2 || b > 16 || y < 1 || y > 20) 
+    {
+        cout << "Error 1" << endl;
+        return 1;
+    }
+
+    cin >> N;
+
+    if (N <= 0 || N >= 1) 
+    {
+        cout << "Error 2" << endl;
+        return 1;
+    }
+
+    string result = ConvertB(N, b, y);
+    cout << result << endl;
+
+    return 0;
+}
---
+++ Лабараторная 3/LabC++3.4/LabC++3.4.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+double ConvertD(string N, int b) 
+{
+    double result = 0.0;
+
+    for (int i = 0; i < N.length(); ++i) 
+    {
+        char c = N[i];
+        int value;
+
+        if (c >= '0' && c <= '9') 
+        {
+            value = c - '0';
+        }
+        else if (c >= 'a' && c <= 'f') 
+        {
+            value = c - 'a' + 10;
+        }
+        else 
+        {
+            cout << "Error 3" << endl;
+            return -1;
+        }
+
+        if (value >= b) 
+        {
+            cout << "Error 4" << endl;
+            return -1;
+        }
+        result += value * (1.0 / pow(b, i + 1));
+    }
+
+    return result;
+}
+
+int main() 
+{
+    int b;
+    string N;
+    cin >> b;
+
+    if (b < 2 || b > 16) 
+    {
+        cout << "Error 1" << endl;
+        return 1;
+    }
+
+    cin >> N;
+
+    if (N.length() > 50 || N[0] != '0' || N[1] != '.') 
+    {
+        cout << "Error 2" << endl;
+        return 1;
+    }
+
+    double decValue = ConvertD(N.substr(2), b);
+    if (decValue == -1)
+    {
+        return 1;
+    }
+
+    cout << fixed << setprecision(5);
+    cout << decValue << endl;
+
+    return 0;
+}
